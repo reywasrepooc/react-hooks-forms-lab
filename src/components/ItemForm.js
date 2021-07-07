0377d3d11bf8c02f6f86a9a49e04d54792242d99
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-07 14:49:19 -0400

    did my part

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ onItemFormSubmit }) {
+  const [formData, setFormData] = useState({
+    id: uuid(),
+    name: '',
+    category: 'Produce'
+  })
+
+  const handleFormChange = event => {
+    const key = event.target.name
+    const value = event.target.value
+    setFormData({
+      ...formData,
+      [key]: value
+    })
+  }
+
+  const handleFormSubmit = event => {
+    event.preventDefault()
+    onItemFormSubmit(formData)
+    setFormData({
+      id: uuid(),
+      name: "",
+      category: "Produce"
+    })
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleFormSubmit} >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" 
+        name="name"
+        value={formData.name}
+        onChange={handleFormChange}
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category"
+        value={formData.category}
+        onChange={handleFormChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" >Add to List</button>
     </form>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,7 +3,7 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [search, setSearch] = useState("")
 
@@ -13,7 +13,7 @@ function ShoppingList({ items }) {
   const handleSearchChange = (e) => {
     setSearch(e.target.value)
   }
-
+ 
   const itemsByCategory = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
@@ -33,9 +33,11 @@ function ShoppingList({ items }) {
     }
   }
 
+ 
+
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
       <Filter 
         onCategoryChange={handleCategoryChange}
         onSearchChange={handleSearchChange}
